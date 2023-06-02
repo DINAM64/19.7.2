6401909e37c05dfd02c20bb2872b1564c041585b
DINAM64 <124585451+DINAM64@users.noreply.github.com>
2023-06-02 15:52:09 +0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,5 @@
+valid_email = "dinam64rus@gmail.com"
+valid_password = "@Dinam3duff22"
+
+# ключ взятый из Swagger для теста
+invalid_auth_key = {"key": "39e3df1f016fedf94495924fde06af84874f2f23dbd0b964abcd9995"}
